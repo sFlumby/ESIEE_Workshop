7a3896d5ab5a931a820e884e4a36f5dba4a52d33
sFlumby <florian.schmitz@edu.esiee-it.fr>
2025-04-01 20:28:35 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+# Simple Math
+
+x = 150
+y = 2
+
+z = x + y
+
+print(x)
+print(y)
+
+#Python using indents
+
+a = 10
+b = 5
+c = a / b
+d = a * b
+e = a % b
+
+name = "anna"
+surname = "Grey"
+
+student_name = name + " " + surname
+
+print(student_name)
+
+
+#Task 1
+# 1. Write IF ELSE statement to validate if x is larger than y then return YES if true
+
+if x > y :
+    print("yes")
+elif x <= y : #or "else :"
+    print("No")
+#
